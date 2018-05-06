0ff2e3c171994396f62c8f4db0463c7335456f42
Jonas Collberg <jonas.collberg@gmail.com>
2018-05-06 13:34:36 +0200

    Change function order

--- src/cljs/tictactoe/views.cljs
+++ src/cljs/tictactoe/views.cljs
@@ -5,6 +5,58 @@
 
 (def cell-size "50px")
 
+(defn cell [index]
+  (let [board     (rf/subscribe [::subs/board])
+        winner    (rf/subscribe [::subs/winner])
+        in-replay (rf/subscribe [::subs/in-replay])
+        cell      (get @board index)]
+    [:div.button.is-dark.title.is-4
+     {:disabled (or @winner @in-replay)
+      :style    {:width  cell-size
+                 :height cell-size
+                 :margin "2px"}
+      :on-click #(when (and (not @winner) (not @in-replay))
+                   (rf/dispatch [::events/make-move index]))}
+     [:div.animated.bounceIn {:hidden (not cell)}
+      cell]]))
+
+(defn board []
+  [:div (for [row (range 3)]
+          ^{:key [row]}
+          [:div (for [col (range 3)]
+                  ^{:key [row col]}
+                  [cell (+ (* 3 row) col)])])])
+
+(defn game-info []
+  (let [current-move (rf/subscribe [::subs/current-move])
+        winner       (rf/subscribe [::subs/winner])
+        next-to-move (rf/subscribe [::subs/next-to-move])]
+    [:div.card.has-text-left.has-text-centered #_{:style {:height "150px"}}
+     [:div.card-content.is-centered
+      "Moves: " @current-move [:br]
+      (if @winner
+        [:span "Game over " [:br] "Winner: " @winner]
+        [:span "Next to move: " @next-to-move])]]))
+
+(defn buttons []
+  (let [current-move (rf/subscribe [::subs/current-move])
+        winner       (rf/subscribe [::subs/winner])
+        in-replay    (rf/subscribe [::subs/in-replay])]
+    [:div.buttons
+     [:button.button.is-danger.tooltip.is-rounded
+      {:disabled (or (zero? @current-move) @in-replay)
+       :data-tooltip (str "Clear game board. Only available when a move has "
+                          "been made")
+       :on-click #(rf/dispatch [::events/initialize-db])}
+      [:span.icon [:i.fas.fa-redo {:aria-hidden true}]]
+      [:span "Reset"]]
+     [:button.button.is-warning.tooltip.is-rounded
+      {:disabled (or (not @winner) @in-replay)
+       :data-tooltip "Replay game. Only available when a game is finished"
+       :on-click #(rf/dispatch [::events/replay-tick 0])}
+      [:span.icon [:i.fas.fa-backward {:aria-hidden true}]]
+      [:span "Replay"]]]))
+
 (defn about-link []
   (let [in-replay (rf/subscribe [::subs/in-replay])]
     [:button#about-button.button
@@ -40,58 +92,6 @@
          "https://github.com/joncol/tictactoe"]]]
       [:footer.modal-card-foot
        "© 2018 Jonas Collberg. No rights reserved."]]]))
-
-(defn cell [index]
-  (let [board     (rf/subscribe [::subs/board])
-        winner    (rf/subscribe [::subs/winner])
-        in-replay (rf/subscribe [::subs/in-replay])
-        cell      (get @board index)]
-    [:div.button.is-dark.title.is-4
-     {:disabled (or @winner @in-replay)
-      :style    {:width  cell-size
-                 :height cell-size
-                 :margin "2px"}
-      :on-click #(when (and (not @winner) (not @in-replay))
-                   (rf/dispatch [::events/make-move index]))}
-     [:div.animated.bounceIn {:hidden (not cell)}
-      cell]]))
-
-(defn game-info []
-  (let [current-move (rf/subscribe [::subs/current-move])
-        winner       (rf/subscribe [::subs/winner])
-        next-to-move (rf/subscribe [::subs/next-to-move])]
-    [:div.card.has-text-left.has-text-centered #_{:style {:height "150px"}}
-     [:div.card-content.is-centered
-      "Moves: " @current-move [:br]
-      (if @winner
-        [:span "Game over " [:br] "Winner: " @winner]
-        [:span "Next to move: " @next-to-move])]]))
-
-(defn board []
-  [:div (for [row (range 3)]
-          ^{:key [row]}
-          [:div (for [col (range 3)]
-                  ^{:key [row col]}
-                  [cell (+ (* 3 row) col)])])])
-
-(defn buttons []
-  (let [current-move (rf/subscribe [::subs/current-move])
-        winner       (rf/subscribe [::subs/winner])
-        in-replay    (rf/subscribe [::subs/in-replay])]
-    [:div.buttons
-     [:button.button.is-danger.tooltip.is-rounded
-      {:disabled (or (zero? @current-move) @in-replay)
-       :data-tooltip (str "Clear game board. Only available when a move has "
-                          "been made")
-       :on-click #(rf/dispatch [::events/initialize-db])}
-      [:span.icon [:i.fas.fa-redo {:aria-hidden true}]]
-      [:span "Reset"]]
-     [:button.button.is-warning.tooltip.is-rounded
-      {:disabled (or (not @winner) @in-replay)
-       :data-tooltip "Replay game. Only available when a game is finished"
-       :on-click #(rf/dispatch [::events/replay-tick 0])}
-      [:span.icon [:i.fas.fa-backward {:aria-hidden true}]]
-      [:span "Replay"]]]))
 
 (defn main-panel []
   [:div.section.animated.fadeIn
